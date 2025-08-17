644aa845593e292ca6b8f8fd6cb55ae5bb870202
Raphael Regis de Araujo <raph4life17@gmail.com>
2025-08-17 18:29:25 -0300

    feat: adição do modal para atualizar clientes no home

--- src/app/features/clientes/pages/clientes-home/clientes-home.ts
+++ src/app/features/clientes/pages/clientes-home/clientes-home.ts
@@ -2,28 +2,53 @@ import { Component } from '@angular/core';
 import { ClientesLista } from '../../components/clientes-lista/clientes-lista';
 import { ModalCadastroCliente } from '../../components/modal-cadastro-cliente/modal-cadastro-cliente';
 import { ClientesBarraDePesquisa } from "../../components/clientes-barra-de-pesquisa/clientes-barra-de-pesquisa";
+import { ModalAtualizarCliente } from '../../components/modal-atualizar-cliente/modal-atualizar-cliente';
+import { Cliente } from '../../models/Cliente';
 
 @Component({
   selector: 'app-clientes-home',
-  imports: [ClientesLista, ModalCadastroCliente, ClientesBarraDePesquisa],
+  imports: [ClientesLista, ModalCadastroCliente, ClientesBarraDePesquisa, ModalAtualizarCliente],
   templateUrl: './clientes-home.html',
   styleUrl: './clientes-home.scss'
 })
 export class ClientesHome {
 
-  valor = false;
+  cliente_selecionado: Cliente = {
+    id: -1,
+    nome: '',
+    id_fiscal: '',
+    Tipo_Pessoa: '',
+    celular: ''
+  };
+  cadastrar_cliente = false;
+  atualizar_cliente = false;
 
-  constructor(){
-    this.valor = false;
+
+  constructor() {
+    this.cadastrar_cliente = false;
+    this.atualizar_cliente = false;
   }
 
   // Abre o modal para cadastrar clientes
   cadastrarCliente() {
-    this.valor = true;
+    this.cadastrar_cliente = true;
   }
 
   // Fecha o modal para cadastrar clientes
-  fecharCadastroCliente(){
-    this.valor = false;
+  fecharCadastroCliente() {
+    this.cadastrar_cliente = false;
+  }
+
+  // Abre modal para atualizar cliente
+  atualizarCliente(cliente: Cliente) {
+    console.log("Cliente recebido no home: " + JSON.stringify(cliente));
+    this.cliente_selecionado = cliente;
+    this.atualizar_cliente = true;
+  }
+
+
+  // Fecha modal para atualizar cliente
+  fecharAtualizarCliente() {
+    this.atualizar_cliente = false;
   }
 }
