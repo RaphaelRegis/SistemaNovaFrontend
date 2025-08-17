b058812502f51c97307ed7cf96ba255e90796987
Raphael Regis de Araujo <raph4life17@gmail.com>
2025-08-17 18:49:40 -0300

    feat: o formulario para editar cliente inicia com os dados atuais dele

--- src/app/features/clientes/components/form-atualizar-cliente/form-atualizar-cliente.ts
+++ src/app/features/clientes/components/form-atualizar-cliente/form-atualizar-cliente.ts
@@ -1,5 +1,6 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormControl, FormGroup, ɵInternalFormsSharedModule, ReactiveFormsModule } from '@angular/forms';
+import { Cliente } from '../../models/Cliente';
 
 @Component({
   selector: 'app-form-atualizar-cliente',
@@ -7,7 +8,9 @@ import { FormControl, FormGroup, ɵInternalFormsSharedModule, ReactiveFormsModul
   templateUrl: './form-atualizar-cliente.html',
   styleUrl: './form-atualizar-cliente.scss'
 })
-export class FormAtualizarCliente {
+export class FormAtualizarCliente implements OnInit {
+
+  @Input() cliente!: Cliente;
 
   // formulario do cliente
   formularioCliente = new FormGroup({
@@ -16,15 +19,24 @@ export class FormAtualizarCliente {
     celular: new FormControl("")
   });
 
+  // usa o input do cliente para colocar os dados atuais no formulario
+  ngOnInit(): void {
+    this.formularioCliente = new FormGroup({
+      nome: new FormControl(this.cliente.nome),
+      id_fiscal: new FormControl(this.cliente.id_fiscal),
+      celular: new FormControl(this.cliente.celular)
+    })
+  }
+
   // botoes do formulário
   @Output() desabilitarEdicao = new EventEmitter<boolean>
 
-  salvar(){
-    console.log("Salvou");
+  salvar() {
+    console.log("Salvou: " + JSON.stringify(this.cliente));
     this.desabilitarEdicao.emit();
   }
 
-  cancelar(){
+  cancelar() {
     console.log("Cancelou");
     this.desabilitarEdicao.emit();
   }
