57b967a55720ec820e0038873299b46eee37cff9
Raphael Regis de Araujo <raph4life17@gmail.com>
2025-08-18 13:38:05 -0300

    feat: lista de endereços

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ClientesHome } from './features/clientes/pages/clientes-home/clientes-home';
+import { EnderecosLista } from "./features/enderecos/components/enderecos-lista/enderecos-lista";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ClientesHome],
+  imports: [RouterOutlet, ClientesHome, EnderecosLista],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
